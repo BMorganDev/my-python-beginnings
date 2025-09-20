3287648e71ee22b6df3f41f51cd4f49c57fa7280
BMorganDev <GOVTECHWITHB@ICLOUD.COM>
2025-09-20 14:14:51 -0500

    Create day1_intro.py

---
+++ day1_intro.py
@@ -0,0 +1,12 @@
+print("Hello Bonnie!")
+print("Welcome to Day 1 of coding.")
+
+name = "Bonnie"
+age = 29
+goal = "to become a software engineer and make 200,000 my first year"
+hobby = "baking and making anything creative"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My goal is", goal)
+print("In my free time, I love", hobby)
